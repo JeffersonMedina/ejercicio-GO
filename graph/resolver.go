c653faf0e59234036a520c276b91c3627cc575fa
Jefferson Medina <medina_pein@hotmail.com>
2021-03-17 12:27:59 -0500

    Ejercicio de GraphQL Server / API en Golang

---
+++ graph/resolver.go
@@ -0,0 +1,11 @@
+package graph
+
+import "github.com/JeffersonMedina/ejercicio-GO/graph/model"
+
+// This file will not be regenerated automatically.
+//
+// It serves as dependency injection for your app, add any dependencies you require here.
+//go:generate go run github.com/99designs/gqlgen
+type Resolver struct {
+	characters []*model.Character
+}
